3238202c80c0b89c2e9a800363b376a2ae21b40f
Taturick <noturno2drick@gmail.com>
2023-08-14 17:24:44 -0300

    fixando findIndex

---
+++ findIndex.js
@@ -0,0 +1,11 @@
+const carros = [
+    {nome: "Hb20", marca: "Hyundai", ano: 2023, cor: "prata"}, 
+    {nome: "Fusca", marca: "Volkswagem", ano: 1971, cor: "vermelho"}, 
+    {nome: "Gol", marca: "Volkswagem", ano: 2007, cor: "prata"},
+    {nome: "Saveiro", marca: "Volkswagem", ano: 2008, cor: "branco"}, 
+    {nome: "Argo", marca: "Fiat", ano: 2022, cor: "preto"} 
+    ]
+
+console.log(carros.findIndex((carro)=>{
+    return carro.nome === "Fusca"
+}))    
